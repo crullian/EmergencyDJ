101ad2ef9f1cfa98baa0a6aaa057b0cd50e796e7
crullian <crullian@gmail.com>
2015-03-13 18:41:15 -0400

    Left audio player working

--- browser/js/common/directives/deck/deck.js
+++ browser/js/common/directives/deck/deck.js
@@ -1,13 +1,18 @@
 'use strict';
-app.directive('deck', function($rootScope) {
+
+app.controller('SearchCtrl', function($scope, AudioFactory) {
+  $scope.search = AudioFactory.search();
+});
+
+app.directive('deck', function(AudioFactory) {
   return {
     restrict: 'E',
     scope: {
-      deck: "="
+      search: "&"
     },
     templateUrl: 'js/common/directives/deck/deck.html'
       // link: function(scope, element, attrs) {
-
-    // }
+      //   scope.search = AudioFactory.search();
+      // }
   };
 });
--- browser/js/common/factories/AudioFactory.js
+++ browser/js/common/factories/AudioFactory.js
@@ -1,15 +1,79 @@
+'use strict';
 app.factory('AudioFactory', function() {
   return {
-    search: function() {
-      var request = gapi.client.youtube.search.list({
-        q: q,
-        part: 'snippet'
-      });
-      request.execute(function(response) {
-        var title = JSON.stringify(response.items[0].snippet.title);
-        var vidId = JSON.stringify(response.items[0].id.videoId);
-        $('#search-container1').html('<h3>' + title + '</h3><br />' + '<h4>' + vidId + '</h4>');
-      });
+    // search: search
+  };
+});
+
+var vidId;
+var player;
+
+var onYouTubeApiLoad = function() {
+  gapi.client.setApiKey('AIzaSyAz3W_FsqnyZUL9ImA8BrBeInXpnCk7Kv8');
+};
+
+function onClientLoad() {
+  gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+};
+
+function search() {
+  var q = $('#query').val();
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet'
+  });
+  request.execute(function(response) {
+    var title = response.items[0].snippet.title;
+    vidId = response.items[0].id.videoId;
+    console.log("Title: ", title, "Video ID: ", vidId);
+    $('#search-container').html('<h3>' + title + '</h3>');
+    player.cueVideoById(vidId, 0, "default");
+  });
+}
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "http://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('youtubeplayer', {
+    height: '100',
+    width: '175',
+    videoId: "",
+    events: {
+      // 'onReady': onPlayerReady
+      // 'onStateChange': onPlayerStateChange
     }
-  }
-})
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+// function onPlayerReady(event) {
+//   event.target.stopVideo();
+// }
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+// var done = false;
+
+// function onPlayerStateChange(event) {
+//   if (event.data == YT.PlayerState.PLAYING && !done) {
+//     setTimeout(stopVideo, 6000);
+//     done = true;
+//   }
+// }
+
+function play() {
+  player.playVideo();
+}
+
+function stop() {
+  player.stopVideo();
+}
